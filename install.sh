c9e8aaaf230e3d915074bcae2c9fc5d7795a425a
AndrewSmithCell <AndrewSmithCell@outlook.com>
2024-11-10 10:55:54 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 mkdir -p downloaded
 cd downloaded
-pip download --platform manylinux1_x86_64 --platform win32 --only-binary=:all: -r ../requirements/requirements1.txt
+cat requirements.txt | xargs -n 1 pip download --platform manylinux1_x86_64 --platform win32 --only-binary=:all:
 cd ..
 python pypi-registry.py
 git config user.name github-actions
